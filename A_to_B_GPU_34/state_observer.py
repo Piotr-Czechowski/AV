543e04332c22e5b793509ef06819579b6680e281
Twoje Imię i Nazwisko <ppiotr.czechowski@gmail.com>
2024-11-15 16:00:10 +0100

    state observer

--- A_to_B_GPU_34/state_observer.py
+++ A_to_B_GPU_34/state_observer.py
@@ -9,6 +9,8 @@ class StateObserver:
         self.action = None
         self.reward = None
         self.episode_step = None
+        self.image = None
+
     def save_to_disk(self, image, episode, step):
         # # Convert the image to a format that OpenCV can work with (BGR format)
         # img_array = np.frombuffer(image.raw_data, dtype=np.uint8)
@@ -24,7 +26,29 @@ class StateObserver:
 
         # # Put text on the image
         # cv2.putText(img, text, position, font, font_scale, color, thickness)
-        image.save_to_disk(f"A_to_B_GPU_34/images/{episode}/{int(step)}.jpeg")
+        self.image = image
+        self.image.save_to_disk(f"A_to_B_GPU_34/images/{episode}/{int(step)}.jpeg")
 
-    def draw_related_values(self):
-        pass
+    def draw_related_values(self, episode, step):
+        # Tworzenie obrazu image2 o wymiarach 80x80 (kolor niebieski)
+        image_sub = np.zeros((80, 80, 3), dtype=np.uint8)
+        image_sub[:] = (255, 0, 0)  # Kolor niebieski w formacie BGR
+
+        # Dodawanie tekstu "Image1" na obraz image1
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        # Dodawanie tekstu "Image1" na obraz image1
+        if self.reward:
+            cv2.putText(image_sub, "Reward:", (3, 10), font, 0.3, (255, 255, 255), 1, cv2.LINE_AA)
+            cv2.putText(image_sub, f"{self.reward}", (60, 10), font, 0.3, (255, 255, 255), 1, cv2.LINE_AA)
+
+        # Dodawanie tekstu "Image2" na obraz image2
+        cv2.putText(image_sub, "Action:", (3, 30), font, 0.3, (255, 255, 255), 1, cv2.LINE_AA)
+        cv2.putText(image_sub, f"{self.action}", (60, 30), font, 0.3, (255, 255, 255), 1, cv2.LINE_AA)
+
+
+        cv2.putText(image_sub, "Timnestamp:", (3, 50), font, 0.3, (255, 255, 255), 1, cv2.LINE_AA)
+        cv2.putText(image_sub, f"{self.image.timestamp}", (3, 60), font, 0.3, (255, 255, 255), 1, cv2.LINE_AA)
+
+        image_sub = np.vstack((image_sub,))
+
+        cv2.imwrite(f"A_to_B_GPU_34/images/{episode}/{int(step)}_s.jpeg", image_sub)
